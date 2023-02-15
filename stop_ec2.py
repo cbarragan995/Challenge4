ff99b1c7b25b0cec7c66df6c4a20125bafc8ca2d
Cristian Barragan <58236658+cbarragan995@users.noreply.github.com>
2023-02-15 00:27:00 -0500

    Update stop_ec2.py

--- stop_ec2.py
+++ stop_ec2.py
@@ -1,7 +1,7 @@
-importar  boto3
-región  =  'us-este-1'
-instancias  = [ 'identificador-instancia' , 'identificador-instancia' ]
-ec2  =  boto3 . cliente ( 'ec2' , nombre_región = región )
-def  lambda_handler ( evento , contexto ):
-    ec2 . stop_instances ( InstanciaIds = instancias )
-    print ( 'detuvo sus instancias: ' +  str ( instancias ))
+import boto3
+region = 'us-east-1'
+instances = ['identificador-instancia', 'identificador-instancia']
+ec2 = boto3.client('ec2', region_name=region)
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=instances)
+    print('stopped your instances: '+ str(instances))
