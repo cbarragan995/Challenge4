55739d46075d42ed746d5fcccbf7274561171ff2
Cristian Barragan <58236658+cbarragan995@users.noreply.github.com>
2023-02-15 00:26:41 -0500

    Update start_ec2.py

--- start_ec2.py
+++ start_ec2.py
@@ -1,7 +1,7 @@
-importar  boto3
-región  =  'us-este-1'
-instancias  = [ 'identificador-instancia' , 'identificador-instancia' ]
-ec2  =  boto3 . cliente ( 'ec2' , nombre_región = región )
-def  lambda_handler ( evento , contexto ):
-    ec2 . start_instances ( InstanciaIds = instancias )
-    print ( 'comenzó sus instancias: ' +  str ( instancias ))
+import boto3
+region = 'us-east-1'
+instances = ['identificador-instancia', 'identificador-instancia']
+ec2 = boto3.client('ec2', region_name=region)
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: '+ str(instances))
